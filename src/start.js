5de39a56ad8226614196f1e4ca7abe37e9d9d9bd
vvp <bossbison@mail.ru>
2023-12-12 18:03:14 +0700

    Add EnvVar unload. Refactor code

--- src/start.js
+++ src/start.js
@@ -6,30 +6,27 @@
  */
 'use strict';
 
-const path = require('path');
+const { join } = require('path');
 
 global.__src = __dirname;
-global.__base = path.join(__dirname, '..');
-global.__public = path.join(__dirname, 'webserver/public');
+global.__base = join(__dirname, '..');
+global.__public = join(__dirname, 'webserver/public');
 
 const config = require('../conf/live.json');
 module.exports.config = config;
 
-const env = require('./classes/EnvVar');
-
-// setup environment vars
+let env = require('./classes/EnvVar');
 env.init(config);
 
 const packageJson = require('../package.json');
-const logger = require('./classes/Logger')('start');
+const logger = require('./classes/Logger')('Start');
 const nginxrtmp = require('./classes/Nginxrtmp')(config);
 const restreamerApp = require('./webserver/app');
 const Restreamer = require('./classes/Restreamer');
-// const RestreamerData = require('./classes/RestreamerData');
 
 
 if (process.env.RS_DEBUG == "true") {
-    logger.info('Debugging enabled. Check the /debug path in the web interface.', false);
+    logger.info('Debugging enabled. Check the /debug path in the web interface.');
 }
 
 // show start message
@@ -47,26 +44,43 @@ logger.info('', false);
 
 // list environment variables
 env.list(logger);
+env.reset();
 
 // bail out if there are errors
 if (env.hasErrors()) {
     process.exit();
 }
 
+env = undefined;
+if (delete require.cache[require.resolve('./classes/EnvVar')] === false) logger.error('Can`t unload EnvVar');
+//logger.debug(`Unload EnvVar: ${delete require.cache[require.resolve('./classes/EnvVar')]}`);
+
+// {
+//     const RestreamerData = require('./classes/RestreamerData');
+//     RestreamerData.checkJSONDb()
+// }
+
 // start the app
-// RestreamerData.checkJSONDb()
 require('child_process').fork('./src/classes/RestreamerData.js')
-    .on('exit', () => {
-        restreamerApp.startWebserver()
+    .on('close', (code) => {
+        if (code) {
+            logger.inf?.(`RestreamerData exit code:${code}`);
+            return;
+        }
+
+        restreamerApp.startWebserver(Restreamer.data)
             .then(() => {
                 Restreamer.bindWebsocketEvents();
                 return nginxrtmp.start(process.env.RS_HTTPS == "true");
             })
             .then(() => {
-                Restreamer.restoreProcesses();
+                return Restreamer.restoreProcesses();
             })
             .catch(error => {
-                logger.error('Error starting webserver and nginx for application: ' + error);
+                logger.err?.('Error starting webserver and nginx for application: ' + error);
+                logger.err?.(`${error.stack}`)
             })
+    })
+    .on('error', (error) => {
+        logger.err?.(error.message)
     });
-
