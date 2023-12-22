695f520239421e1a493063edcb8ef413b0d17677
vvpx <bossbiso@mail.ru>
2023-12-22 12:25:23 +0700

    build action - cache ffmpeg information

---
+++ contrib/get-ffdata.js
@@ -0,0 +1,28 @@
+//@ts-check
+'use strict'
+
+const { promisify } = require('node:util')
+const path = require('node:path')
+const fs = require('node:fs').promises
+const ff = require('fluent-ffmpeg')
+const index = require.resolve('fluent-ffmpeg')
+
+const formats = promisify(ff.getAvailableFormats)
+const encoders = promisify(ff.getAvailableEncoders)
+const cacheFile = 'cache.json'
+const data = {}
+
+ff.setFfmpegPath('/usr/local/bin/ffmpeg')
+ff.setFfprobePath('/usr/local/bin/ffprobe')
+
+async function buildCache() {
+    [data.formats, data.encoders] = await Promise.all([formats(), encoders()])
+    return fs.writeFile(path.join(path.dirname(index), 'lib', cacheFile), JSON.stringify(data))
+}
+
+buildCache()
+    .then(() => console.log(`"${cacheFile}" created succefully`))
+    .catch(e => {
+        console.error(e)
+        process.exit(-1)
+    })
