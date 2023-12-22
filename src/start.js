d8c28361f980decd95aa9925c1315dc580af9efb
vvpx <bossbiso@mail.ru>
2023-12-22 09:57:34 +0700

    refactor code

--- src/start.js
+++ src/start.js
@@ -1,86 +1,64 @@
-/**
- * @file this file is loaded on application start and initializes the application
- * @link https://github.com/datarhei/restreamer
- * @copyright 2015 datarhei.org
- * @license Apache-2.0
- */
-'use strict';
+// @ts-check
+'use strict'
 
-const { join } = require('path');
+const { join } = require('node:path')
+const { version } = require('../package.json')
+globalThis.appVersion = version
+globalThis.__src = __dirname
+globalThis.__base = join(__dirname, '..')
+globalThis.__public = join(__dirname, 'webserver/public')
+const config = globalThis.appConfig = require('../conf/live.json')
+const env = require('./classes/EnvVar')
+env.init(config)
 
-global.__src = __dirname;
-global.__base = join(__dirname, '..');
-global.__public = join(__dirname, 'webserver/public');
+const logger = require('./classes/Logger')('Start')
+const nginxrtmp = require('./classes/Nginxrtmp')()
+const app = require('./webserver/app')
+const Restreamer = require('./classes/Restreamer')
 
-const config = require('../conf/live.json');
-module.exports.config = config;
-
-let env = require('./classes/EnvVar');
-env.init(config);
-
-const packageJson = require('../package.json');
-const logger = require('./classes/Logger')('Start');
-const nginxrtmp = require('./classes/Nginxrtmp')(config);
-const restreamerApp = require('./webserver/app');
-const Restreamer = require('./classes/Restreamer');
-
-
-if (process.env.RS_DEBUG == "true") {
-    logger.info('Debugging enabled. Check the /debug path in the web interface.');
+if (process.env.RS_DEBUG?.toLowerCase() === "true") {
+    logger.info('Debugging enabled. Check the /debug path in the web interface.')
 }
 
 // show start message
-logger.info('     _       _             _           _ ', false);
-logger.info('  __| | __ _| |_ __ _ _ __| |___   ___(_)', false);
-logger.info(' / _  |/ _  | __/ _  |  __|  _  |/  _ | |', false);
-logger.info('| (_| | (_| | || (_| | |  | | | |  __/| |', false);
-logger.info('|_____|_____|_||_____|_|  |_| |_|____||_|', false);
-logger.info('', false);
-logger.info('Restreamer v' + packageJson.version, false);
-logger.info('', false);
-logger.info('ENVIRONMENTS', false);
-logger.info('More information in our Docs', false);
-logger.info('', false);
+logger.info('     _       _             _           _ ', false)
+logger.info('  __| | __ _| |_ __ _ _ __| |___   ___(_)', false)
+logger.info(' / _  |/ _  | __/ _  |  __|  _  |/  _ | |', false)
+logger.info('| (_| | (_| | || (_| | |  | | | |  __/| |', false)
+logger.info('|_____|_____|_||_____|_|  |_| |_|____||_|', false)
+logger.info('', false)
+logger.info('Restreamer v' + version, false)
+logger.info('', false)
+logger.info('ENVIRONMENTS', false)
+logger.info('More information in our Docs', false)
+logger.info('', false)
 
 // list environment variables
-env.list(logger);
-env.reset();
+env.list(logger)
+if (env.hasErrors()) process.exit()
+logger.debug(`Unload EnvVar: ${delete require.cache[require.resolve('./classes/EnvVar')]}`)
 
-// bail out if there are errors
-if (env.hasErrors()) {
-    process.exit();
-}
-
-env = undefined;
-if (delete require.cache[require.resolve('./classes/EnvVar')] === false) logger.error('Can`t unload EnvVar');
-//logger.debug(`Unload EnvVar: ${delete require.cache[require.resolve('./classes/EnvVar')]}`);
-
-// {
-//     const RestreamerData = require('./classes/RestreamerData');
-//     RestreamerData.checkJSONDb()
-// }
-
-// start the app
-require('child_process').fork('./src/classes/RestreamerData.js')
-    .on('close', (code) => {
+require('child_process')
+.fork('./src/classes/RestreamerData.js')
+    .on('close', code => {
         if (code) {
-            logger.inf?.(`RestreamerData exit code:${code}`);
-            return;
+            logger.inf?.(`RestreamerData exit code:${code}`)
+            return
         }
 
-        restreamerApp.startWebserver(Restreamer.data)
+        app.startWebserver(Restreamer.data)
             .then(() => {
-                Restreamer.bindWebsocketEvents();
-                return nginxrtmp.start(process.env.RS_HTTPS == "true");
+                Restreamer.bindWebsocketEvents()
+                return nginxrtmp.start(process.env.RS_HTTPS === "true")
             })
             .then(() => {
-                return Restreamer.restoreProcesses();
+                return Restreamer.restoreProcesses()
             })
             .catch(error => {
-                logger.err?.('Error starting webserver and nginx for application: ' + error);
+                logger.err?.('Error starting webserver and nginx for application: ' + error)
                 logger.err?.(`${error.stack}`)
             })
     })
-    .on('error', (error) => {
+    .on('error', error => {
         logger.err?.(error.message)
-    });
+    })
