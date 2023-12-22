3f8ec80869323f75a181dab279d9650afd491550
vvpx <bossbiso@mail.ru>
2023-12-22 10:16:38 +0700

    many changes

--- src/webserver/app.js
+++ src/webserver/app.js
@@ -1,64 +1,48 @@
-/**
- * @link https://github.com/datarhei/restreamer
- * @copyright 2015 datarhei.org
- * @license Apache-2.0
- */
-"use strict";
+"use strict"
 
 // express
-const express = require("express");
-const session = require("express-session");
-const cookie = require("cookie");
-const cookieParser = require("cookie-parser");
-// const bodyParser = require("body-parser");
-// const compression = require("compression");
+const express = require("express")
+const session = require("express-session")
+const cookie = require("cookie")
+const cookieParser = require("cookie-parser")
+// const bodyParser = require("body-parser")
+// const compression = require("compression")
 
 // other
-const path = require("path");
-// const Q = require('../classes/MyQ.js');
-//import * as Q from "../classes/MyQ.mjs"
-
-const crypto = require("crypto");
+const path = require("path")
+const crypto = require("crypto")
 
 // modules
-const logger = require.main.require("./classes/Logger")("Webserver");
-const indexRouter = require("./controllers/index");
-const apiV1 = require("./controllers/api/v1");
+const logger = require("../classes/Logger")("Webserver")
+const indexRouter = require("./controllers/index")
+const apiV1 = require("./controllers/api/v1")
 
 // middleware
 // const expressLogger = require("./middleware/expressLogger");
 
 // socket.io
-const { Server } = require("socket.io");
-// const { resolve } = require("path");
+const { Server } = require("socket.io")
 
-/**
- * Class for the ReStreamer webserver, powered by express.js
- */
+
+/**Class for the ReStreamer webserver, powered by express.js*/
 class RestreamerExpressApp {
-    v1;
-    app;
 
-    /**
-     * constructs a new express app with prod or dev config
-     */
+    /**constructs a new express app with prod or dev config*/
     constructor() {
-        this.v1 = new apiV1();
-        this.app = express();
-        this.secretKey = crypto.randomBytes(16).toString("hex");
-        this.sessionKey = "restreamer-session";
-        this.sessionStore = new session.MemoryStore();
+        this.v1 = new apiV1()
+        this.app = express()
+        this.secretKey = crypto.randomBytes(16).toString("hex")
+        this.sessionKey = "restreamer-session"
+        this.sessionStore = new session.MemoryStore()
 
         if (process.env.RS_NODEJS_ENV === "dev") {
-            this.initDev();
+            this.initDev()
         } else {
-            this.initProd();
+            this.initProd()
         }
     }
 
-    /**
-     * use sessions for the express app
-     */
+    /**use sessions for the express app*/
     useSessions() {
         this.app.use(
             session({
@@ -67,73 +51,52 @@ class RestreamerExpressApp {
                 key: this.sessionKey,
                 secret: this.secretKey,
                 unset: "destroy",
-                store: this.sessionStore,
+                store: this.sessionStore
             })
-        );
+        )
     }
 
-    /**
-     * add automatic parsers for the body
-     */
+    /**add automatic parsers for the body*/
     addParsers() {
-        this.app.use(express.json()); // bodyParser.json());
-        this.app.use(cookieParser());
+        this.app.use(express.json()) // bodyParser.json())
+        this.app.use(cookieParser())
     }
 
-    /**
-     * add content compression on responses
-     */
+    /**add content compression on responses*/
     addCompression() {
-        // this.app.use(compression());
+        // this.app.use(compression())
     }
 
-    /**
-     * add express logger
-     */
+    /**add express logger*/
     addExpressLogger() {
-        // this.app.use("/", expressLogger);
+        // this.app.use("/", expressLogger)
     }
 
-    /**
-     * beautify json response
-     */
+    /**beautify json response*/
     beautifyJSONResponse() {
-        this.app.set("json spaces", 4);
+        this.app.set("json spaces", 4)
     }
 
-    /**
-     * create a promise to check when websockets are ready for bindings
-     */
-    // createPromiseForWebsockets() {
-    //     this.app.set("websocketsReady", Q.defer());
-    // }
-
-    /**
-     * add the restreamer routes
-     */
+    /**add the restreamer routes*/
     addRoutes() {
-        indexRouter(this.app);
-        this.app.use("/v1", this.v1.router);
+        indexRouter(this.app)
+        this.app.use("/v1", this.v1.router)
     }
 
-    /**
-     * add 404 error handling on pages, that have not been found
-     */
+    /**add 404 error handling on pages, that have not been found*/
     // add404ErrorHandling() {
     //     this.app.use((req, res, next) => {
-    //         const err = new Error("Not Found " + req.url);
-    //         err.status = 404;
-    //         next(err);
-    //     });
+    //         const err = new Error("Not Found " + req.url)
+    //         err.status = 404
+    //         next(err)
+    //     })
     // }
 
-    /**
-     * add ability for internal server errors
-     */
+    /**add ability for internal server errors*/
     // add500ErrorHandling() {
     //     this.app.use((err, req, res, next) => {
-    //         logger.error(err);
-    //         res.status(err.status || 500);
+    //         logger.error(err)
+    //         res.status(err.status || 500)
     //         res.send({
     //             message: err.message,
     //             error: {},
@@ -141,37 +104,33 @@ class RestreamerExpressApp {
     //     });
     // }
 
-    /**
-     * enable websocket session validation
-     */
+    /**enable websocket session validation*/
     secureSockets() {
         const val = (handshakeData, accept) => {
             if (handshakeData.headers.cookie) {
                 this.sessionStore.get(
                     cookieParser.signedCookie(
-                        cookie.parse(handshakeData.headers.cookie)[
-                        this.sessionKey
-                        ],
+                        cookie.parse(handshakeData.headers.cookie)[this.sessionKey],
                         this.secretKey
                     ),
                     (err, s) => {
                         if (!err && s && s.authenticated) {
-                            return accept(null, true);
+                            return accept(null, true)
                         }
                     }
-                );
+                )
             } else {
-                return accept(null, false);
+                return accept(null, false)
             }
-        };
+        }
 
         this.app.get("io").use(function (socket, next) {
             val(socket.request, function (err, authorized) {
-                if (err) return next(new Error(err));
-                if (!authorized) return next(new Error("Not authorized"));
-                next();
-            });
-        });
+                if (err) return next(new Error(err))
+                if (!authorized) return next(new Error("Not authorized"))
+                next()
+            })
+        })
     }
 
     /**
@@ -179,64 +138,61 @@ class RestreamerExpressApp {
      * @returns {*|promise}
      */
     startWebserver(dataSrc) {
-        this.v1.setSrcData(dataSrc);
+        this.v1.setSrcData(dataSrc)
+        logger.info("Starting ...")
+        this.app.set("port", process.env.RS_NODEJS_PORT)
+
         return new Promise(resolve => {
-            let server = null;
-            logger.info("Starting ...");
-            this.app.set("port", process.env.RS_NODEJS_PORT);
-            server = this.app.listen(this.app.get("port"), () => {
-                this.app.set("io", new Server(server, { path: "/socket.io" }));
-                this.secureSockets();
-                this.app.set("server", server.address());
+            const server = this.app.listen(this.app.get("port"), '127.0.0.1', () => {
+                this.app.set("io", new Server(server, { path: "/socket.io" }))
+                this.secureSockets()
+                this.app.set("server", server.address())
+                // logger.dbg?.(`app."server": ${server.address()}`);
 
                 // promise to avoid ws binding before the webserver has been started
                 // this.app.get("websocketsReady").resolve(this.app.get("io"));
-                logger.info("Running on port " + process.env.RS_NODEJS_PORT);
+                logger.inf?.("Running on port " + process.env.RS_NODEJS_PORT)
                 resolve()
-            });
-        });
+            })
+        })
     }
 
-    /**
-     * stuff that have always to be added to the webapp
-     */
+    /**stuff that have always to be added to the webapp*/
     initAlways() {
-        this.useSessions();
-        this.addParsers();
-        this.addCompression();
-        // this.addExpressLogger();
-        this.beautifyJSONResponse();
-        // this.createPromiseForWebsockets();
-        this.addRoutes();
+        this.useSessions()
+        this.addParsers()
+        this.addCompression()
+        // this.addExpressLogger()
+        this.beautifyJSONResponse()
+        // this.createPromiseForWebsockets()
+        this.addRoutes()
     }
 
-    /**
-     * prod config for the express app
-     */
+    /**prod config for the express app*/
     initProd() {
-        logger.debug("Init webserver with PROD environment");
-        this.initAlways();
+        logger.debug("Init webserver with PROD environment")
+        this.initAlways()
         this.app.get("/", (_req, res) => {
-            res.sendFile(path.join(global.__public, "index.prod.html"));
-        });
+            res.sendFile(path.join(global.__public, "index.prod.html"))
+        })
+
         // Internal error handling exist in express
-        // this.add404ErrorHandling();
-        // this.add500ErrorHandling();
+        // this.add404ErrorHandling()
+        // this.add500ErrorHandling()
     }
 
-    /**
-     * dev config for the express app
-     */
+    /**dev config for the express app*/
     initDev() {
-        logger.debug("Init webserver with DEV environment");
-        this.initAlways();
+        logger.debug("Init webserver with DEV environment")
+        this.initAlways()
         this.app.get("/", (req, res) => {
-            res.sendFile(path.join(global.__public, "index.dev.html"));
-        });
-        // this.add404ErrorHandling();
-        // this.add500ErrorHandling();
+            res.sendFile(path.join(global.__public, "index.dev.html"))
+        })
+
+        // this.add404ErrorHandling()
+        // this.add500ErrorHandling()
     }
 }
 
-const restreamerApp = new RestreamerExpressApp();
-module.exports = restreamerApp;
+const restreamerApp = new RestreamerExpressApp
+module.exports = restreamerApp
