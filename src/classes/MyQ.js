da8c21b088f15048634e6685e90f7be84b0be502
vvpx <bossbison@mail.ru>
2024-06-23 13:46:34 +0700

    update MyQ.js

--- src/classes/MyQ.js
+++ src/classes/MyQ.js
@@ -1,65 +1,35 @@
-'use strict'
+'use strict';
 
-// const EventEmitter = require('node:events').EventEmitter
-
-/**
- * Replace Q.deferred object
- */
-// class Qdefer {
-//     #__event;
-//     #resolution_promise;
-
-//     constructor() {
-//         this.#__event = new EventEmitter();
-
-//         this.#resolution_promise = new Promise((resolve, reject) => {
-//             this.#__event.once('q', args => (args[0] ? resolve : reject)(args[1]));
-//         });
-//     }
-
-//     /**
-//      * @param {boolean} bResolve - promise resolution
-//      */
-//      #resolution(bResolve, value) {
-//         if (this.#__event) {
-//             this.#__event.emit('q', [bResolve, value]);
-//             this.#__event = null;
-//         }
-//     }
-
-//     reject(reason) { this.#resolution(false, reason); }
-
-//     resolve(value) { this.#resolution(true, value); }
-
-//     get promise() { return this.#resolution_promise; }
-// };
 
 class Qdefer {
-    #resolve
-    #reject
+    #resolve;
+    #reject;
 
     constructor() {
+        this.#resolve = null;
+        this.#reject = null;
+
         this.promise = new Promise((resolve, reject) => {
-            this.#resolve = resolve
-            this.#reject = reject
+            this.#resolve = resolve;
+            this.#reject = reject;
         })
     }
 
     reject(reason) {
-        this.#reject?.(reason)
-        this.#resolve = null
-        this.#reject = null
+        this.#reject?.(reason);
+        this.#resolve = null;
+        this.#reject = null;
     }
 
     resolve(value) {
-        this.#resolve?.(value)
-        this.#resolve = null
-        this.#reject = null
+        this.#resolve?.(value);
+        this.#resolve = null;
+        this.#reject = null;
     }
 
-    dispose() { this.promise = null }
-    
+    dispose() { this.promise = null; }
+
     // get promise() { return this.#resolution_promise }
 }
 
-module.exports.defer = () => new Qdefer
+module.exports.defer = () => new Qdefer;
