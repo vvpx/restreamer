c171389d4537a1d900968047ee6639bcc075b098
vvpx <bossbison@mail.ru>
2024-07-09 17:41:01 +0700

    add paths to cache.json

--- contrib/get-ffdata.js
+++ contrib/get-ffdata.js
@@ -1,28 +1,31 @@
 //@ts-check
-'use strict'
+'use strict';
 
-const { promisify } = require('node:util')
-const path = require('node:path')
-const fs = require('node:fs').promises
-const ff = require('fluent-ffmpeg')
-const index = require.resolve('fluent-ffmpeg')
+const { promisify } = require('node:util');
+const path = require('node:path');
+const fs = require('node:fs').promises;
+const ff = require('fluent-ffmpeg');
+const index = require.resolve('fluent-ffmpeg');
 
-const formats = promisify(ff.getAvailableFormats)
-const encoders = promisify(ff.getAvailableEncoders)
-const cacheFile = 'cache.json'
-const data = {}
+const formats = promisify(ff.getAvailableFormats);
+const encoders = promisify(ff.getAvailableEncoders);
+const cacheFile = 'cache.json';
+const data = {
+    ffmpegPath: '/usr/local/bin/ffmpeg',
+    ffprobePath: '/usr/local/bin/ffprobe'
+};
 
-ff.setFfmpegPath('/usr/local/bin/ffmpeg')
-ff.setFfprobePath('/usr/local/bin/ffprobe')
+ff.setFfmpegPath(data.ffmpegPath);
+ff.setFfprobePath(data.ffprobePath);
 
 async function buildCache() {
-    [data.formats, data.encoders] = await Promise.all([formats(), encoders()])
-    return fs.writeFile(path.join(path.dirname(index), 'lib', cacheFile), JSON.stringify(data))
+    [data.formats, data.encoders] = await Promise.all([formats(), encoders()]);
+    return fs.writeFile(path.join(path.dirname(index), 'lib', cacheFile), JSON.stringify(data));
 }
 
 buildCache()
     .then(() => console.log(`"${cacheFile}" created succefully`))
     .catch(e => {
-        console.error(e)
-        process.exit(-1)
+        console.error(e);
+        process.exit(-1);
     })
