4f9bdc217aa249c3f25c0812bfcfd292848a8df8
vvpx <bossbiso@mail.ru>
2024-09-06 15:53:11 +0700

    Refactor WebsocketsController

--- src/classes/WebsocketsController.js
+++ src/classes/WebsocketsController.js
@@ -6,48 +6,51 @@
  * @typedef {import("socket.io").Socket} Socket
 */
 
-const ctx = 'wsControl';
-var logger = require('./Logger')(ctx);
-var app = require("../webserver/app").app;
+const logger = require('./Logger')('wsControl');
+const app = require("../webserver/app").app;
 /**@type {Server} */
 var io;
-let connections = 0;
+var connections = 0;
+var _callback;
 
 /**
  * static class websocket controller, that helps communicating through websockets to different namespaces and ensures
  * that websocket events are bound, if the websocket server has been initialized (through promise made on app start)
  */
-class WebsocketsController {
 
-    /**
-     * emit an event to WS
-     * @param {string} event name of the event
-     * @param {object} data data to emit to the client event listener
-     */
-    static emit(event, data) {
-        io.sockets.emit(event, data);
-    }
 
-    /**
-     * @returns {((event: string, data: object) => void) | null}
-     */
-    static emitOnConnections() {
-        return connections > 0 ? this.emit : null;
-    }
-
-    /**
-     * add callback on WS connection
-     * @param {(arg: Socket) => void} callback
-     */
-    static setConnectCallback(callback) {
-        io ??= app.get('io');
-        io.on('connection', socket => {
-            ++connections;
-            logger.inf?.(`Connection from ${socket.handshake.headers['x-forwarded-for']}`);
-            socket.once("disconnect", (reason) => logger.inf?.(`${socket.handshake?.headers['x-forwarded-for']} disconnected: ${reason} *${--connections}`));
-            callback(socket);
-        });
-    }
+/**
+ * emit an event to WS
+ * @param {string} event name of the event
+ * @param {object} data data to emit to the client event listener
+ */
+function emit(event, data) {
+    return io.sockets.emit(event, data);
 }
 
-module.exports = WebsocketsController;
+
+/**
+ * add callback on WS connection
+ * @param {(arg: Socket) => *} callback
+ */
+function setConnectCallback(callback) {
+    io ??= app.get('io');
+    _callback = callback;
+
+    io.on('connection', socket => {
+        ++connections;
+        logger.inf?.(`Connection from ${socket.handshake.headers['x-forwarded-for']}`);
+        socket.once("disconnect", /**@this {Socket}*/ function disconnectCb(reason) {
+            logger.inf?.(`${this.handshake?.headers['x-forwarded-for']} disconnected: ${reason} *${--connections}`);
+        });
+        
+        _callback(socket);
+    });
+}
+
+
+module.exports = {
+    emit,
+    emitOnConnections: () => connections > 0 ? emit : null,
+    setConnectCallback
+};
